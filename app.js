9f4ad7f110a8670febc7629397cbda0dda1e4924
ylsv <lisov1988@gmail.com>
2019-09-03 16:02:47 +0300

    add edit and update routes, edit template

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const   express     = require('express'),
-        app         = express(),
-        bodyParser  = require('body-parser'),
-        mongoose    = require('mongoose');
+const   express        = require('express'),
+        app            = express(),
+        bodyParser     = require('body-parser'),
+        mongoose       = require('mongoose'),
+        methodOverride = require('method-override');
 
 // APP CONFIG
-mongoose.connect('mongodb://localhost:27017/restful_blog_app', {useNewUrlParser: true}); 
+mongoose.connect('mongodb://localhost:27017/restful_blog_app', {useNewUrlParser: true, useFindAndModify: false}); 
 app.set('view engine', 'ejs'); 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -75,6 +77,28 @@ app.get('/blogs/:id', function(req, res){
     });
 });
 
+// 5. EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+            console.log('error');
+        } else  
+            // render the edit page template
+            res.render('edit', {blog: foundBlog});
+    });
+});
+
+// 6. UPDATE ROUTE
+app.put('/blogs/:id', function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/' + req.params.id);
+        }
+    });
+});
 
 
 
